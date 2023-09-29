a93566d4bb3bfc81b987d76d899ad23b407c8654
Tony Kay <tony.kay@gmail.com>
2023-09-28 23:06:16 -0700

    Revamp template to use more modern setup (hooks) instead of older create class.

--- src/main/app/client_native.cljs
+++ src/main/app/client_native.cljs
@@ -1,35 +1,92 @@
 (ns app.client-native
-  "Entry point for native client."
+  "Entry point for native client.
+
+   Here are the ideas:
+
+   1. I read the expo startup code. All it does for native mobile apps is call AppRegistry.registerComponent. So, I just
+      do that manually in this ns.
+   2. I used react hooks for a wrapper around what Fulcro wants to render (the arg to render-root). I added local state there so I could
+      make a function to force a re-render of the new UI with the current state
+  "
   (:require
+    ["react-native" :as native :refer [Text Button View AppRegistry Platform]]
+    ["react" :as react]
+    [app.model.session :as session]
     [app.application :refer [SPA]]
-    [com.fulcrologic.fulcro-native.expo-application-40 :as expo]
     [app.mobile-ui.root :as root]
     [app.mobile-ui.authentication :as auth]
-    [app.mobile-ui.resources :as resources]
-    [taoensso.timbre :as log]
-    [com.fulcrologic.fulcro.networking.http-remote :as net]
+    [com.fulcrologic.fulcro.algorithms.react-interop :refer [react-factory]]
     [com.fulcrologic.fulcro.application :as app]
+    [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+    [com.fulcrologic.fulcro.mutations :as m]
+    [com.fulcrologic.fulcro.networking.http-remote :as net]
+    [com.fulcrologic.fulcro.react.hooks :as hooks]
+    [com.fulcrologic.fulcro.rendering.keyframe-render :as kr]
+    [com.fulcrologic.fulcro.routing.dynamic-routing :as dr]
     [com.fulcrologic.fulcro.ui-state-machines :as uism]
-    [app.model.session :as session]))
+    [taoensso.timbre :as log]))
+
+;; Tracking for what Fulcro wants to render. We don't want to close over a constant value because then hot code
+;; reloads would not see updates.
+(defonce root-ref (volatile! nil))
+;; In order for Fulcro to remount things, we have to return the root component from render
+(defonce root-component-ref (volatile! nil))
+
+;; We need a way to externally see the react set state hook function, so we can force the wrapper to re-render at will
+(defonce force-render-atom (volatile! identity))
+(defn force-render!
+  "Update the component local state of the root wrapper to force a root-level re-render."
+  []
+  (comp/enable-forced-refresh! 1000)
+  (@force-render-atom))
+
+(defn render-root
+  "A function that can be used as the main Fulcro render for an expo app. Registers the app on the first render. Any
+   subsequence calls just update a global atom that is tracking the element Fulcro is trying to render, which is used
+   in the closure of the internal generated wrapper. This is so hot code reload refreshes the UI without reloading the app."
+  [root]
+  (try
+    (let [first-call? (nil? @root-ref)]
+      (vreset! root-ref root)
+
+      (if first-call?
+        (let [RootWrapper (fn [this]
+                            (let [body @root-ref
+                                  [n render!] (hooks/use-state 0)]
+                              (vreset! force-render-atom (fn [] (render! (inc n))))
+                              (vreset! root-component-ref this)
+                              (try
+                                (if (fn? body)
+                                  (body)
+                                  body)
+                                (catch :default e
+                                  (log/error e "Render failed")))))]
+          (native/AppRegistry.registerComponent "main" (fn [] RootWrapper)))
+        (do
+          (@force-render-atom)
+          @root-component-ref)))
+    (catch :default e
+      (log/error e "Unable to mount/refresh"))))
 
 ;; See defines in shadow-cljs for dev mode
 (goog-define SERVER_URL "http://production.server.com/api-native")
 
-(defn ^:export start
-  {:dev/after-load true}
-  []
-  (log/info "Re-mounting")
-  (app/mount! @SPA root/Root :i-got-no-dom-node))
+;; Modify the Fulcro app to use our React-native rendering wrapper
+(defonce APP
+  (app/fulcro-app
+    {:render-root!      render-root
+     :hydrate-root!     (fn [& _])
+     :optimized-render! kr/render!
+     :remotes           {:remote (net/fulcro-http-remote {:url        SERVER_URL
+                                                          :make-xhrio #(doto (net/make-xhrio)
+                                                                         (.setWithCredentials true))})}}))
 
-(defn init []
-  (reset! SPA (expo/fulcro-app
-                {:cached-images    resources/images
-                 :cached-fonts     resources/fonts
-                 :client-did-mount (fn [app]
-                                     (uism/begin! app session/session-machine ::session/session
-                                                  {:actor/login-form      auth/Login
-                                                   :actor/current-session session/Session}))
-                 :remotes          {:remote (net/fulcro-http-remote {:url        SERVER_URL
-                                                                     :make-xhrio #(doto (net/make-xhrio)
-                                                                                    (.setWithCredentials true))})}}))
-  (start))
+(defn start []
+  (reset! SPA APP)
+  (app/mount! APP root/Root :ignored-in-native)
+  (uism/begin! APP session/session-machine ::session/session
+    {:actor/login-form      auth/Login
+     :actor/current-session session/Session}))
+
+
+(defn ^:export init [] (start))
--- src/main/app/mobile_ui/root.cljs
+++ src/main/app/mobile_ui/root.cljs
@@ -24,46 +24,14 @@
                    :routes (array
                              #js {:key "first" :title "Main"}
                              #js {:key "second" :title "Settings"})}]
-    (b/ui-tab-view {:navigationState state
-                    :renderScene     (b/ui-scene-map #js {:first  main-route
-                                                          :second settings-route})
-                    :key             "ui-tab-view-unique"
-                    :renderTabBar    (fn [props]
-                                       (b/ui-container {:mt   "10"
-                                                        :maxW "100%"
-                                                        :w    "100%"}
-                                                       (b/ui-hstack {:justifyContent "space-between"
-                                                                     :alignItems     "center"
-                                                                     :w              "100%"}
-                                                                    (b/ui-hstack {} "")
-                                                                    (b/ui-hstack {} "")
-                                                                    (b/ui-hstack {} (b/ui-button {:onPress     (fn []
-                                                                                                                 (uism/trigger! this ::session/session :event/logout)
-                                                                                                                 (dr/change-route this ["login"]))
-                                                                                                  :hasText     true
-                                                                                                  :transparent true} "Logout")))
-                                                       (b/ui-box {:flexDirection "row"
-                                                                  :w             "100%"}
-                                                                 (doall
-                                                                   (map-indexed
-                                                                     (fn [idx route]
-                                                                       (let [color (if (= @index idx) "#e5e5e5" "#1f2937")
-                                                                             border-color (if (= @index idx) "cyan.500" "gray.400")]
-                                                                         (b/ui-box {:borderBottomWidth "3"
-                                                                                    :borderColor       border-color
-                                                                                    :flex              "1"
-                                                                                    :alignItems        "center"
-                                                                                    :p                 "3"
-                                                                                    :mt                "5"
-                                                                                    :cursor            "pointer"
-                                                                                    :key               (str "ui-box: fn route: " route ": idx" idx)}
-                                                                                   (b/ui-pressable {:onPress (fn []
-                                                                                                               (reset! index idx))}
-                                                                                                   (b/ui-animated-text {:style color}
-                                                                                                                       (get route "title"))))))
-                                                                     (get-in (js->clj props) ["navigationState" "routes"]))))))
-                    :onIndexChange   (fn [idx]
-                                       (reset! index idx))})))
+    (b/ui-box
+      {:padding 20}
+      (b/ui-button {:onPress     (fn []
+                                   (uism/trigger! this ::session/session :event/logout)
+                                   (dr/change-route this ["login"]))
+                    :hasText     true
+                    :transparent true} "Logout")
+      (b/ui-text {} "Hello world"))))
 
 (defrouter RootRouter [_ _]
   {:router-targets [Login Main]})
@@ -75,5 +43,6 @@
    :componentDidMount (fn [this]
                         (dr/change-route this ["login"]))
    :initial-state     {:root/router {}}}
-  (b/ui-base {:mt "50px"}
-             (ui-root-router router)))
+  (when (seq router)
+    (b/ui-base {:mt "50px"}
+      (ui-root-router router))))
